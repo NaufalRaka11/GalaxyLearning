7e4ef438a95024e4abe6780cd6c5a0d0dd0be586
NaufalRaka11 <141594686+NaufalRaka11@users.noreply.github.com>
2026-03-03 22:40:15 +0700

    Update mathSections with new content and structure

--- js/data.js
+++ js/data.js
@@ -1,2 +1,5 @@
-export const mathSections = [ /* isi seperti sebelumnya */ ];
+export const mathSections = [
+  name:"Bilangan",icon:"🪐",
+  questions:[
+    {q:'14.607 berarti...',a:'14 ribu + 600 + 7', options:['14 ribu + 600 + 7','1 ribu + 4607','140 + 67','146 + 7']}]}];
 export const englishSections = [ /* isi seperti sebelumnya */ ];
