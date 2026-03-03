361c30f2932df7f65112c22e865330dbce370c64
NaufalRaka11 <141594686+NaufalRaka11@users.noreply.github.com>
2026-03-03 22:42:50 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 export const mathSections = [
-  name:"Bilangan",icon:"🪐",
-  questions:[
-    {q:'14.607 berarti...',a:'14 ribu + 600 + 7', options:['14 ribu + 600 + 7','1 ribu + 4607','140 + 67','146 + 7']}]}];
+  {
+    name: "Bilangan",
+    icon: "🪐",
+    questions: [
+      { q: '14.607 berarti...', a: '14 ribu + 600 + 7', options: ['14 ribu + 600 + 7', '1 ribu + 4607', '140 + 67', '146 + 7'] }
+    ]
+  }
+];
 export const englishSections = [ /* isi seperti sebelumnya */ ];
